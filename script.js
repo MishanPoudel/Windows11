0adfd5fe61b8b17693664587fbec7fe83a6d28ee
MishanPoudel <poudelmishan2@gmail.com>
2023-11-05 22:10:04 +0545

    still working on taskbar(right side)

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ start.addEventListener("click", ()=> {
         startmenu.style.bottom = "50px"
     }
 })
+
+
+function updateTime() {
+    const currentDateTime = new Date();
+
+    // Get the date in MM/DD/YYYY format
+    const dateOptions = { month: '2-digit', day: '2-digit', year: 'numeric' };
+    const date = currentDateTime.toLocaleDateString([], dateOptions);
+
+    const timeOptions = { hour: '2-digit', minute: '2-digit' };
+    const time = currentDateTime.toLocaleTimeString([], timeOptions);
+
+    const timeContainer = document.querySelector('.time');
+    const dateContainer = document.querySelector('.date');
+
+    timeContainer.textContent = `${time}`;
+    dateContainer.textContent = `${date}`;
+}
+
+updateTime(); // Call the function immediately to display the time and date
+setInterval(updateTime, 1000); // Update time and date every second

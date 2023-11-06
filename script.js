c70e7d752a3294393da10cef4416dd24a77c78cf
MishanPoudel <poudelmishan2@gmail.com>
2023-11-06 13:23:13 +0545

    taskbar completed

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-let start = document.getElementsByClassName("start")[0]
-let startmenu = document.getElementsByClassName("startmenu")[0]
+let start = document.getElementsByClassName("start")[0];
+let startmenu = document.getElementsByClassName("startmenu")[0];
 
-start.addEventListener("click", ()=> {
-    if (startmenu.style.bottom == "50px"){
-        startmenu.style.bottom = "-655px"
-    }else{
-        startmenu.style.bottom = "50px"
-    }
-})
-
+start.addEventListener("click", () => {
+  if (startmenu.style.bottom == "60px") {
+    startmenu.style.bottom = "-655px";
+  } else {
+    startmenu.style.bottom = "60px";
+  }
+});
 
 function updateTime() {
-    const currentDateTime = new Date();
+  const currentDateTime = new Date();
 
-    const dateOptions = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    const date = currentDateTime.toLocaleDateString([], dateOptions);
+  const dateOptions = { month: "2-digit", day: "2-digit", year: "numeric" };
+  const date = currentDateTime.toLocaleDateString([], dateOptions);
 
-    const timeOptions = { hour: '2-digit', minute: '2-digit' };
-    const time = currentDateTime.toLocaleTimeString([], timeOptions);
+  const timeOptions = { hour: "2-digit", minute: "2-digit" };
+  const time = currentDateTime.toLocaleTimeString([], timeOptions);
 
-    const timeContainer = document.querySelector('.time');
-    const dateContainer = document.querySelector('.date');
+  const timeContainer = document.querySelector(".time");
+  const dateContainer = document.querySelector(".date");
 
-    timeContainer.textContent = `${time}`;
-    dateContainer.textContent = `${date}`;
+  timeContainer.textContent = `${time}`;
+  dateContainer.textContent = `${date}`;
 }
 
-updateTime(); 
-setInterval(updateTime, 1000); 
+updateTime();
+setInterval(updateTime, 1000);
